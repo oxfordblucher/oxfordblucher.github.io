6e52027230e864f85cfe9f373323454fd5a52833
oxfordblucher <hiuchanhk@gmail.com>
2020-10-28 13:38:58 -0400

    fixed missing links

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#umewe").on("click", function() {
+    window.open("https://oxfordblucher.github.io/Restaurant-Roulette");
+})
+
+$("#wthrDash").on("click", function() {
+    window.open("https://oxfordblucher.github.io/hw6-Weather-Dashboard/");
+})
+
+$("#wkdaySched").on("click", function() {
+    window.open("https://oxfordblucher.github.io/hw5-Workday-Scheduler/");
+})
