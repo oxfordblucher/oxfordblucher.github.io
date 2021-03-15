2d5d7acf0e0651d14bf7503f0f07749248b38c77
oxfordblucher <hiuchanhk@gmail.com>
2021-03-15 13:55:26 -0400

    fixing 404

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import photo from '../assets/personal_photo.jpg';
-import coming from '../assets/coming_soon.jpg';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Figure from 'react-bootstrap/Figure';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 import './Summary.css';
 
 const Summary = () => {
