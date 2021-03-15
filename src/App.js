41313fc6b53223c174e4298e5a9ad0ddfc35f752
oxfordblucher <hiuchanhk@gmail.com>
2021-03-15 14:02:01 -0400

    no idea

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Summary from './pages/Summary';
 import Header from './components/Header';
+import React from 'react';
 
 function App() {
   return (
-    <Router>
+    <React.Fragment>
       <Header />
-      <Switch>
-        <Route exact path='/' component={Summary} />
-      </Switch>
-    </Router>
+      <Summary />
+    </React.Fragment>
   );
 }
 
