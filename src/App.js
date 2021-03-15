49b3a3a8accbd0955d994514940a4e86093ee6ad
oxfordblucher <hiuchanhk@gmail.com>
2021-03-15 13:47:56 -0400

    got rid of nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Summary from './pages/Summary';
-import Projects from './pages/Projects';
-import Connect from './pages/Connect';
 import Header from './components/Header';
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={Summary} />
-        <Route exact path='/projects' component={Projects} />
-        <Route exact path='/connect' component={Connect} />
       </Switch>
     </Router>
   );
